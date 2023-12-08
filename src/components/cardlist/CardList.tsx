86049df30f59779c63ea629f1e5236664174cc7c
sobhanhsa <sobhanb7b7@protonmail.com>
2023-12-08 13:37:21 +0330

    feat:add editors pick and popular posts menu

--- src/components/cardlist/CardList.tsx
+++ src/components/cardlist/CardList.tsx
@@ -9,6 +9,10 @@ const CardList = () => {
             <h1 className={styles.title}>Recent Posts</h1>
             <div className={styles.posts} >
                 <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
             </div>
             <Pagination />
         </div>
