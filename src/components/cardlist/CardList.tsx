e8c554b2bb8f85890b3cb5afd6c946744d9ce480
sobhanhsa <sobhanb7b7@protonmail.com>
2023-12-08 10:52:48 +0330

    fix:adjust card list styles

--- src/components/cardlist/CardList.tsx
+++ src/components/cardlist/CardList.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import Pagination from "../pagination/Pagination";
 import styles from "./cardlist.module.css"
+import Card from "../card/Card";
 
 const CardList = () => {
     return (
         <div className={styles.container}>
-            CardList
+            <h1 className={styles.title}>Recent Posts</h1>
+            <div className={styles.posts} >
+                <Card/>
+            </div>
             <Pagination />
         </div>
     );
